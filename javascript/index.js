45226dd0b3afd1cd6bdd6c93dcf71ec23615bea3
Daenerys97 <49641478+Daenerys97@users.noreply.github.com>
2019-04-15 13:46:54 +0200

    Add files via upload
    getKey read interval werkend 2000 milliseconden

--- javascript/index.js
+++ javascript/index.js
@@ -3,17 +3,20 @@
 
 // configuratie
 var server = "http://www.cmd.camp:12345"; // het adres van de server
-var getKey = "2aQEMEDk"; // de get key om data uit de server te halen
-var sendKey = "3TMw3wYR"; // de send key om data naar de server te sturen
+var getKey = "ZM5hJfzp"; // de get key om data uit de server te halen
+var sendKey = "7JrpHFhd"; // de send key om data naar de server te sturen
 var frequentie = 2000; // het aantal milliseconden waarop de pagina de data herlaadt
 
-window.setInterval(getData(getKey), frequentie); // laad elk x aantal seconden de getData() functie
 
 // de functie om de data te verkrijgen
-function getData(getKey) {
+function getData() {
+    console.log('De functie getData is gestart', getKey);
     var oReq = new XMLHttpRequest(); // maak een nieuw request aan
     oReq.addEventListener("load", function () { // als de request is geladen, voer dan deze functie uit
         var data = this.responseText;
+
+        console.log(data);
+
         if (data) { // verander de voorwaarden waaraan de data moet voldoen
             // als de data voeldoet aan de voorwaarden, voer dan onderstaande code uit
 
@@ -26,8 +29,10 @@ function getData(getKey) {
     oReq.send(); // verstuur request
 };
 
-function sendData(sendKey, data) {
+function sendData(data) {
     var oReq = new XMLHttpRequest(); // maak een nieuw request aan
     oReq.open("GET", `${server}/send/${sendKey}/${data}`); // definieer het request en de server
     oReq.send(); // verstuur request
 }
+
+window.setInterval(getData, 2000); // laad elk x aantal seconden de getData() functie
